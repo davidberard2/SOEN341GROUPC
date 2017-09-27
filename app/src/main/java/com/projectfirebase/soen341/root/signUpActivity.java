86094291705a6b7a875538c56943ef2743f55a81
davidberard2 <davidberard2@gmail.com>
2017-09-27 15:25:37 -0400

    Adding first pieces of the sign up activity

---
+++ app/src/main/java/com/projectfirebase/soen341/root/signUpActivity.java
@@ -0,0 +1,15 @@
+package com.projectfirebase.soen341.root;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.projectfirebase.soen341.soen341.R;
+
+public class signUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
