4741a7bfe414ce95358e0c4e908acb7a1b705f2b
Karen <karen_bie@hotmail.com>
2017-10-15 16:25:36 -0400

    Layout adjustments, email now properly updates, and added toast displays for unsuccessful profile updates.

--- app/src/main/java/Fragments/ProfileFragment.java
+++ app/src/main/java/Fragments/ProfileFragment.java
@@ -32,9 +32,9 @@ public class ProfileFragment extends Fragment {
     View view;
 
     // TODO:  Refactor to abide by name conventions
-    private TextView name_tv;
-    private TextView email_tv;
-    private TextView phoneNumber_tv;
+    private TextView name_et;
+    private TextView email_et;
+    private TextView phoneNumber_et;
     private ImageView photo_iv;
 
     private ImageButton updatePhoto_ib;
@@ -75,9 +75,9 @@ public class ProfileFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_profile, container, false);
         setHasOptionsMenu(true);
 
-        name_tv = (TextView) view.findViewById(R.id.profile_name);
-        email_tv = (TextView) view.findViewById(R.id.profile_email);
-        phoneNumber_tv = (TextView) view.findViewById(R.id.profile_phone_number);
+        name_et = (TextView) view.findViewById(R.id.profile_name);
+        email_et = (TextView) view.findViewById(R.id.profile_email);
+        phoneNumber_et = (TextView) view.findViewById(R.id.profile_phone_number);
         photo_iv = (ImageView) view.findViewById(R.id.profile_photo);
         updatePhoto_ib = (ImageButton) view.findViewById(R.id.profile_update_photo);
 
@@ -87,25 +87,25 @@ public class ProfileFragment extends Fragment {
             photoUrl = user.getPhotoUrl();
 
             if (name != null) {
-                name_tv.setText(name);
+                name_et.setText(name);
             }
 
             // TODO: Check if user's email is verified?
-            email_tv.setText(email);
+            email_et.setText(email);
 
             if (photoUrl != null) {
                 new DownloadImageTask(photo_iv).execute(photoUrl.toString());
             }
 
-            name_tv.setVisibility(View.VISIBLE);
-            email_tv.setVisibility(View.VISIBLE);
-            phoneNumber_tv.setVisibility(View.VISIBLE);
+            name_et.setVisibility(View.VISIBLE);
+            email_et.setVisibility(View.VISIBLE);
+            phoneNumber_et.setVisibility(View.VISIBLE);
             photo_iv.setVisibility(View.VISIBLE);
             updatePhoto_ib.setVisibility(View.VISIBLE);
         } else {
-            name_tv.setVisibility(View.GONE);
-            email_tv.setVisibility(View.GONE);
-            phoneNumber_tv.setVisibility(View.GONE);
+            name_et.setVisibility(View.GONE);
+            email_et.setVisibility(View.GONE);
+            phoneNumber_et.setVisibility(View.GONE);
             photo_iv.setVisibility(View.GONE);
             updatePhoto_ib.setVisibility(View.GONE);
 
@@ -159,10 +159,10 @@ public class ProfileFragment extends Fragment {
     }
 
     private void updateName() {
-        if (name_tv.getText().toString().trim().equals("")) {
-            Toast.makeText(view.getContext(), "This is not a valid name", Toast.LENGTH_SHORT).show();
+        if (name_et.getText().toString().trim().equals("")) {
+            Toast.makeText(view.getContext(), "Name is invalid", Toast.LENGTH_SHORT).show();
         } else {
-            name = name_tv.getText().toString();
+            name = name_et.getText().toString();
             UserProfileChangeRequest updateName = new UserProfileChangeRequest.Builder()
                     .setDisplayName(name)
                     .build();
@@ -173,23 +173,28 @@ public class ProfileFragment extends Fragment {
                     if (task.isSuccessful()) {
                         Log.d("USER_UPDATE", "User profile name updated.");
                     }
+                    else {
+                        Toast.makeText(view.getContext(), "Name update error", Toast.LENGTH_SHORT).show();
+                    }
                 }
             });
         }
-
     }
 
     private void updateEmail() {
-        if (name_tv.getText().toString().trim().equals("")) {
-            Toast.makeText(view.getContext(), "This is not a valid name", Toast.LENGTH_SHORT).show();
+        if (email_et.getText().toString().trim().equals("")) {
+            Toast.makeText(view.getContext(), "Email is invalid", Toast.LENGTH_SHORT).show();
         } else {
-            email = name_tv.getText().toString();
+            email = email_et.getText().toString();
             user.updateEmail(email)
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
                                 Log.d("USER_UPDATE", "User email updated.");
+                            }
+                            else {
+                                Toast.makeText(view.getContext(), "Email update error", Toast.LENGTH_SHORT).show();
                             }
                         }
                     });
