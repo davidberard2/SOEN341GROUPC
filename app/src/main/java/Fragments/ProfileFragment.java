f5dd6f93f412685f4e75319de885ba02b9128cc7
Karen <karen_bie@hotmail.com>
2017-10-14 21:19:31 -0400

    Modified profile page input and added window adjustment for keyboard display.

--- app/src/main/java/Fragments/ProfileFragment.java
+++ app/src/main/java/Fragments/ProfileFragment.java
@@ -9,9 +9,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -24,11 +26,15 @@ import Tasks.DownloadImageTask;
 
 public class ProfileFragment extends Fragment {
 
+    View view;
+
+    // TODO:  Refactor to abide by name conventions
     private TextView name_tv;
     private TextView email_tv;
     private TextView phoneNumber_tv;
     private ImageView photo_iv;
-    private ImageButton addPhoto_ib;
+
+    private ImageButton updatePhoto_ib;
 
     private String name;
     private String email;
@@ -55,48 +61,43 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+        view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         name_tv = (TextView) view.findViewById(R.id.profile_name);
         email_tv = (TextView) view.findViewById(R.id.profile_email);
         phoneNumber_tv = (TextView) view.findViewById(R.id.profile_phone_number);
         photo_iv = (ImageView) view.findViewById(R.id.profile_photo);
-        addPhoto_ib = (ImageButton) view.findViewById(R.id.profile_add_photo);
+        updatePhoto_ib = (ImageButton) view.findViewById(R.id.profile_update_photo);
 
         if (user != null) {
             name = user.getDisplayName();
             email = user.getEmail();
-//            photoUrl = user.getPhotoUrl();
-            // tmp
-            photoUrl = Uri.parse("http://i0.kym-cdn.com/photos/images/original/000/692/145/49c.png");
-
-            // TODO: Check if user's email is verified?
+            photoUrl = user.getPhotoUrl();
 
             if (name != null) {
                 name_tv.setText(name);
             }
-            else {
-                name_tv.setText("Evan Mateo");
-//                editName();
-//                name = user.getDisplayName();
-//                name_tv.setText(name);
-            }
 
+            // TODO: Check if user's email is verified?
             email_tv.setText(email);
 
-            new DownloadImageTask(photo_iv).execute(photoUrl.toString());
+            if (photoUrl != null) {
+                new DownloadImageTask(photo_iv).execute(photoUrl.toString());
+            }
 
             name_tv.setVisibility(View.VISIBLE);
             email_tv.setVisibility(View.VISIBLE);
             phoneNumber_tv.setVisibility(View.VISIBLE);
             photo_iv.setVisibility(View.VISIBLE);
-            addPhoto_ib.setVisibility(View.VISIBLE);
+            updatePhoto_ib.setVisibility(View.VISIBLE);
         } else {
             name_tv.setVisibility(View.GONE);
             email_tv.setVisibility(View.GONE);
             phoneNumber_tv.setVisibility(View.GONE);
             photo_iv.setVisibility(View.GONE);
-            addPhoto_ib.setVisibility(View.GONE);
+            updatePhoto_ib.setVisibility(View.GONE);
 
             // TODO: Display message telling user that they are currently not logged in. Suggest signing up or logging in.
         }
@@ -109,18 +110,16 @@ public class ProfileFragment extends Fragment {
     public void onStart() {
         super.onStart();
 
-        // TODO: Set listener for add_photo ImageButton to call addPhoto()
-        addPhoto_ib.setOnClickListener(new View.OnClickListener() {
+        updatePhoto_ib.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                editPhoto();
+                updatePhoto();
             }
         });
-
-        // TODO: Set listener for edit_email, edit_phone_number
+        // TODO: Add a single listener for text field modifications
     }
 
-    private void editPhoto() {
+    private void updatePhoto() {
         UserProfileChangeRequest updatePhoto = new UserProfileChangeRequest.Builder()
                 .setPhotoUri(Uri.parse("https://firebasestorage.googleapis.com/v0/b/projectfirebase-9323d.appspot.com/o/test_profile_photo.jpg?alt=media&token=8653a2a4-37e4-4534-a9b0-3de3c54f14c2"))
                 .build();
@@ -129,24 +128,49 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful()) {
-                    Log.d("update_user", "User profile photo updated.");
+                    Log.d("USER_UPDATE", "User profile photo updated.");
                 }
             }
         });
     }
 
-    private void editName() {
-        UserProfileChangeRequest updateName = new UserProfileChangeRequest.Builder()
-                .setDisplayName("Evan Mateo") // TODO: Implement an "Edit Name" fragment
-                .build();
+    private void updateName() {
+        if(name_tv.getText().toString().trim().equals("")) {
+            Toast.makeText(view.getContext(), "This is not a valid name", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            name = name_tv.getText().toString();
+            UserProfileChangeRequest updateName = new UserProfileChangeRequest.Builder()
+                    .setDisplayName(name)
+                    .build();
 
-        user.updateProfile(updateName).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if (task.isSuccessful()) {
-                    Log.d("update_user", "User profile name updated.");
+            user.updateProfile(updateName).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()) {
+                        Log.d("USER_UPDATE", "User profile name updated.");
+                    }
                 }
-            }
-        });
+            });
+        }
+
+    }
+
+    private void updateEmail() {
+        if(name_tv.getText().toString().trim().equals("")) {
+            Toast.makeText(view.getContext(), "This is not a valid name", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            email = name_tv.getText().toString();
+            user.updateEmail(email)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("USER_UPDATE", "User email updated.");
+                            }
+                        }
+                    });
+        }
     }
 }
