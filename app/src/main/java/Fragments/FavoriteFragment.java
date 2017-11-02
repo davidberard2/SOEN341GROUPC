ffb68f031db8831258315adcbceb123f90f008b7
EvanMateo <gevanmateo@gmail.com>
2017-11-02 11:36:17 -0400

    Add fragment listing page

--- app/src/main/java/Fragments/FavoriteFragment.java
+++ app/src/main/java/Fragments/FavoriteFragment.java
@@ -3,22 +3,54 @@ package Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.projectfirebase.soen341.root.Adapters.ListItemAdapter;
+import com.projectfirebase.soen341.root.Listing;
 import com.projectfirebase.soen341.root.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class FavoriteFragment extends Fragment {
+
+    private FirebaseAuth authRef = FirebaseAuth.getInstance();
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+    private DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+    private DatabaseReference itemsRef = rootRef.child("Items");
+    private DatabaseReference myUser = rootRef.child("Users");
+    private DatabaseReference favRef;
+
+
+
+    private ArrayList<Listing> listingsList = new ArrayList<>();
+    private List<String> favList;
+    private RecyclerView recyclerView;
+    private ListItemAdapter mAdapter;
+
+    private boolean isViewFiltered;
+    private String filterString;
+
 
 
     public FavoriteFragment() {
         // Required empty public constructor
-    }
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
     }
 
     public static FavoriteFragment newInstance() {
@@ -26,11 +58,80 @@ public class FavoriteFragment extends Fragment {
         return fragment;
     }
 
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+
+        if(user != null) {
+            favRef = myUser.child(user.getUid());
+
+            favRef.addValueEventListener(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    String favString = dataSnapshot.child("Favorites").getValue(String.class);
+                    favList = Arrays.asList(favString.split(";"));
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+
+
+            recyclerView = (RecyclerView) view.findViewById(R.id.fav_recycler_view);
+            mAdapter = new ListItemAdapter(listingsList);
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+            recyclerView.setLayoutManager(mLayoutManager);
+            recyclerView.setAdapter(mAdapter);
+            itemsRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    listingsList.clear();
+
+                    Map<String, Object> itemsMap = (HashMap<String, Object>) dataSnapshot.getValue();
+
+                    for (String key : itemsMap.keySet()) {
+                        if (favList.contains(key)) {
+                            Object itemMap = itemsMap.get(key);
+                            if (itemMap instanceof Map) {
+                                Map<String, Object> itemObj = (Map<String, Object>) itemMap;
+
+                                String id = key;
+                                String name = (String) itemObj.get("Name");
+                                Double price = ((Number) itemObj.get("Price")).doubleValue();
+                                String url = (String) itemObj.get("ImageURL");
+
+                                Listing item = new Listing(key, name, price, url);
+
+                                listingsList.add(item);
+
+                            }
+                        }
+                    }
+                    mAdapter.notifyDataSetChanged();
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                }
+            });
+        }
+
+        return view;
 
     }
 
