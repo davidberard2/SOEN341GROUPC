2ed9c8f045c633e12cd5a0c7be98cbe06f7a985d
Karen <karen_bie@hotmail.com>
2017-11-26 06:01:32 -0500

    Fixes.

--- app/src/main/java/com/projectfirebase/soen341/root/LoginActivity.java
+++ app/src/main/java/com/projectfirebase/soen341/root/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.projectfirebase.soen341.root;
 
 import android.content.Intent;
-import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
